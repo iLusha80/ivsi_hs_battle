e9827c994219b0fb65b2a0cecfef973cea02d6f4
i80 <ivsberx@gmail.com>
2025-02-02 02:07:50 +0300

    Добавления типа существ

--- app/routers.py
+++ app/routers.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import render_template, request, redirect, session, jsonify
+
 from .database import get_db
 from . import app
+from .services.search import super_search
 
 
 @app.route('/')
@@ -40,6 +42,8 @@ def index():
     LIMIT 5
     ''').fetchall()
 
+
+
     if total_score > 0:
         txt_score = f'-{total_score}'
         score_color = 'color:red'
@@ -53,12 +57,15 @@ def index():
     with open('app/static/data/heroes.txt', 'r') as f:
         hero_list = f.read().splitlines()
 
+    unit_types = cursor.execute('''SELECT id, name FROM unit_types ORDER BY id''').fetchall()
+
     return render_template('index.html',
                            games=games,
                            score=txt_score,
                            score_color=score_color,
                            hero_list=hero_list,
-                           scores_5day=scores_5day)
+                           scores_5day=scores_5day,
+                           unit_types=unit_types)
 
 
 @app.route('/add-game', methods=['POST'])
@@ -69,6 +76,8 @@ def add_game():
     try:
         p1_place = int(request.form['p1_place'])
         p2_place = int(request.form['p2_place'])
+        p1_unit_type = int(request.form['p1_unit_type'])
+        p2_unit_type = int(request.form['p2_unit_type'])
 
         if not (1 <= p1_place <= 8 and 1 <= p2_place <= 8):
             raise ValueError("Invalid position value")
@@ -77,13 +86,15 @@ def add_game():
         cursor = db.cursor()
         cursor.execute('''
             INSERT INTO games 
-            (player1_hero, player1_place, player2_hero, player2_place)
-            VALUES (?, ?, ?, ?)
+            (player1_hero, player1_place, player2_hero, player2_place, player1_unit_type, player2_unit_type)
+            VALUES (?, ?, ?, ?, ?, ?)
         ''', (
             request.form['p1_hero'].strip(),
             p1_place,
             request.form['p2_hero'].strip(),
-            p2_place
+            p2_place,
+            p1_unit_type,
+            p2_unit_type,
         ))
         db.commit()
 
@@ -108,3 +119,20 @@ def logout():
 @app.route('/tests')
 def tests():
     return render_template('tests.html')
+
+# endpoint для тестов поиска Принимает строку возвращает список Похожих ответов. ответы будут передаваться из функции super_search
+
+@app.route('/search', methods=['GET'])
+def search():
+    db = get_db()
+    search_string = request.args.get('q')
+    print(search_string)
+    if not search_string:
+        return 'Search string is required'
+
+    # Запускаем функцию супер поиска и получаем список похожих ответов
+    similar_answers = super_search(search_string, db)
+    print(similar_answers)
+
+
+    return jsonify(list(similar_answers))
