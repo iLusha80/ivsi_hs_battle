54d23a142f59233ea540193359101536c2a94ee9
i80 <ivsberx@gmail.com>
2025-02-02 18:08:04 +0300

    add config secret_key sqlalch url

--- config.py
+++ config.py
@@ -7,6 +7,9 @@ from dataclasses import dataclass
 @dataclass
 class Config:
     admin_password: str
+    SECRET_KEY: str
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'games.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 def load_config() -> Config:
     load_dotenv()
