c38e1fbff5d7ec0631dab443e2f4a4cfdf359c88
i80 <ivsberx@gmail.com>
2025-02-22 01:25:31 +0300

    add: dowload new data and img. and auto add new heroes in db

--- app/routers/statistic.py
+++ app/routers/statistic.py
@@ -1,39 +1,37 @@
 import dataclasses
 import json
 from typing import Dict, Any
+import importlib
 
 from flask import Blueprint, render_template
-
-from app.services.stats_utils import generate_hero_stats_plot, generate_jiro_stats_plot, generate_unit_types_circle
 
 statistics_bp = Blueprint('statistics', __name__)
 
 @dataclasses.dataclass
 class DataStatBase:
     plot_name: str
-    html_id: int
+    html_id: str
     json_plotly_data: Dict[str, Any]
+    width: int
 
 @statistics_bp.route('/statistics')
 def statistics():
+    with open('app/static/data/statistic_config.json', 'r') as f:
+        config = json.load(f)
 
-    plot_name = 'Тестовое название'
-    html_id = 'tst_plot'
-    hero_plot = generate_hero_stats_plot()
-    top4_data = DataStatBase(plot_name=plot_name, html_id=html_id, json_plotly_data=hero_plot)
-    # jiro plot
-    plot_name = 'Среднее место по героям'
-    html_id = 'jiro_plot'
-    jiro_plot = generate_jiro_stats_plot()
-    jiro_data = DataStatBase(plot_name=plot_name, html_id=html_id, json_plotly_data=jiro_plot)
+    data = []
+    for item in config:
+        module_name = item['module']
+        function_name = item['function']
+        plot_name = item['name']
+        html_id = item['id']
+        width = item['width']
 
-    plot_name = 'Типы Юнитов'
-    html_id = 'unit_types_plot'
-    unit_types_plot = generate_unit_types_circle()
-    unit_types_data = DataStatBase(plot_name=plot_name, html_id=html_id, json_plotly_data=unit_types_plot)
+        module = importlib.import_module(f'app.services.for_stat.{module_name}')
+        generate_plot = getattr(module, function_name)
+        plot_data = generate_plot()
 
-    data = [unit_types_data, top4_data, jiro_data, unit_types_data]
+        data_item = DataStatBase(plot_name=plot_name, html_id=html_id, json_plotly_data=plot_data, width=width)
+        data.append(data_item)
 
-    return render_template('statistics.html',
-                           data_list=data,
-                           )
+    return render_template('statistics.html', data_list=data)
