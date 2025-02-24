975872e0fc419017247f5b8d69661e87d8c6a773
i80 <ivsberx@gmail.com>
2025-02-25 02:28:54 +0300

    add: unit types stat

--- app/routers/for_test.py
+++ app/routers/for_test.py
@@ -1,14 +1,12 @@
 from flask import Blueprint, render_template
-from app.services.for_test.get_players_places import get_player_place_stats
+from app.services.for_test.get_players_unit_stats import get_players_all_unit_stats
 
 for_test_bp = Blueprint('for_test', __name__)
 
 
-
-
-
 @for_test_bp.route('/tests')
 def tests():
-    player1 = get_player_place_stats(p=1)  # Получаем статистику игрока 1
-    player2 = get_player_place_stats(p=2)
-    return render_template('tests.html', data_stat=[player1, player2])
+    players_unit_stat = get_players_all_unit_stats()
+    return render_template('tests.html',
+                           players_unit_stat=players_unit_stat
+                           )
---
+++ app/services/for_test/get_players_unit_stats.py
@@ -0,0 +1,69 @@
+from dataclasses import dataclass
+from typing import Optional, List
+
+from app.services.utils import get_data_from_query
+
+
+@dataclass
+class UnitStat:
+    unit_type_name: str
+    count_games: int
+    avg_place: float
+    count_1place: int
+    percent_1place: float
+    count_top4: int
+    percent_top4: float
+
+@dataclass
+class PlayerStats:
+    player1: List[UnitStat]
+    player2: List[UnitStat]
+
+def get_player_unit_stats(p: int = 1) -> UnitStat:
+    """
+
+    Получает статистику по юнитам игрока.
+
+    :param p: Игрок 1 или 2
+    :type p: int
+    :return:
+    :rtype: PlayerStats
+
+    """
+    sql = f"""
+    WITH cte AS (
+        SELECT
+            ut."name"																				AS unit_type_name
+            ,ROUND(avg(g.player{p}_place), 2)														AS avg_place
+            ,count(*)																				AS count_games
+            ,sum(CASE WHEN g.player{p}_place = 1  THEN 1 ELSE 0 END) 								AS count_1place
+            ,(sum(CASE WHEN g.player{p}_place = 1 THEN 1 ELSE 0 END)::float)/(count(*)::float)		AS percent_1place
+            ,sum(CASE WHEN g.player{p}_place <=4  THEN 1 ELSE 0 END) 								AS count_top4
+            ,(sum(CASE WHEN g.player{p}_place <=4 THEN 1 ELSE 0 END)::float)/(count(*)::float)		AS percent_top4
+        FROM maindata.games g
+        LEFT JOIN maindata.unit_types ut 
+            ON g.player{p}_unit_type_id = ut.id
+        GROUP BY 1
+    )
+    SELECT
+        unit_type_name
+        ,avg_place
+        ,count_games
+        ,count_1place
+        ,ROUND(percent_1place::numeric, 2)	AS percent_1place
+        ,count_top4
+        ,ROUND(percent_top4::numeric, 2)	AS percent_top4
+    FROM cte
+    ORDER BY 2;
+    """
+
+    sql_result = get_data_from_query(sql=sql)
+
+    result = [UnitStat(*row) for row in sql_result]
+    return result
+
+def get_players_all_unit_stats():
+    player1 = get_player_unit_stats(p=1)
+    player2 = get_player_unit_stats(p=2)
+
+    return PlayerStats(player1, player2)
